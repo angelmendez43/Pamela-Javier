80a090d562237b0ef069259be686bf69516f4bfe
angel <angelmendez00512@gmail.com>
2024-02-05 14:26:19 -0600

    hasta carusel

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,37 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
   
+
+
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("carousel-item");
+    let indicators = document.getElementsByClassName("indicator");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < indicators.length; i++) {
+        indicators[i].className = indicators[i].className.replace(" active-indicator", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    indicators[slideIndex-1].className += " active-indicator";
+  }
+  
+  // Actualizar moveSlide para utilizar plusSlides
+  function moveSlide(n) {
+    plusSlides(n);
+  }
+  
